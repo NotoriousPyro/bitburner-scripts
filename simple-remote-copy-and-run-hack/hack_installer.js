bc796f26f90d195a78b7d7f6a1e7f38be9250a2f
NotoriousPyro <craigcrawford1988@gmail.com>
2022-01-02 19:13:48 +0000

    First commit

---
+++ simple-remote-copy-and-run-hack/hack_installer.js
@@ -0,0 +1,20 @@
+export async function main(ns) {
+	var destination = ns.args[0];
+	
+	// If we have the BruteSSH.exe program, use it to open the SSH Port
+	// on the target server
+	if (ns.fileExists("BruteSSH.exe", "home")) {
+		ns.brutessh(destination);
+	}
+
+	// Get root access to target server
+	ns.nuke(destination);
+	await ns.scp("hack.js", "home", destination);
+
+	// Run the hack script
+	var res = ns.exec("hack.js", destination);
+	
+	if (res == 0) {
+		throw "Failed"
+	}
+}
